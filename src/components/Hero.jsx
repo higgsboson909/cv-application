850d53f6e34c1e5b56305b7cb34c1be10a549d0a
higgsboson909 <higgsbosson906@gmail.com>
2025-07-17 15:15:12 +0500

    Start App

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,24 +2,25 @@ import cv_image from "../assets/images/cv.jpg";
 const Hero = () => {
   return (
     <div class="container is-flex is-align-items-center">
+      <div className="column is-1"></div>
       <div className="column is-6 is-flex-column">
         <div className="is-flex is-flex-direction-column left-column">
           <h1 className="is-size-1 has-text-left has-text-weight-bold has-text-grey-darker">
             Build Professional<br></br> Resume
           </h1>
           <div className="has-text-grey-dark">
-            Quickly create a standout CV that highlights your skills — no
-            hassle, all impact.
+            Quickly create a standout CV that highlights your<br></br> skillsl —
+            no hassle, all impact.
           </div>
           <button className="button is-normal has-background-grey-darker">
             Get Started
           </button>
         </div>
       </div>
-      <div className="column is-1"></div>
-      <div className="column is-6">
+      <div className="column is-6 is-justify-content-center">
         <img src={cv_image} alt="CV" />
       </div>
+      <div className="column is-1"></div>
     </div>
   );
 };
